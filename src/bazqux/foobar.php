967379304c77fc65d72922f9c014a5da9924cb69
Eric Jacobson <efjacobs1@gmail.com>
2016-01-10 14:00:41 -0800

    set up directories, link path and robots.txt. will check logs to confirm bots are going to where they are forbidden.

---
+++ src/bazqux/foobar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Slag of the Internet</title>
+
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/includes-header.php";
+      include($path);
+    ?>
+    
+  </head>
+
+  <body>
+    
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/header.php";
+      include($path);
+    ?>
+
+    <div class="wrapper">
+      <div class="container">
+        <div class="row">
+          <h1>That was a mistake.</h1>
+          <h4>You will never return.</h4>
+        </div>
+      </div>
+    </div>
+    
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/footer.php";
+      include($path);
+    ?>
+
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/includes-footer.php";
+      include($path);
+    ?>
+  
+  </body>
+</html>
+
+
--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,5 +1,8 @@
 <footer class="footer">
   <div class="container">
-    <p class="text-center">&copy; Copyright 2015-<?php echo date("Y"); ?>. All Rights Reserved.</p>
+    <p class="text-center">
+      &copy; Copyright 2015-<?php echo date("Y"); ?>. All Rights Reserved.
+      <span><a href="/bazqux"><img src="/img/bazqux/link-image.png"></a></span>
+    </p>
   </div>
 </footer>
